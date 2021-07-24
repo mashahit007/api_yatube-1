ca9142b0b78161f20e23c8c6200cefcc426e5488
coherentus <coherentus@gmail.com>
2021-07-24 14:01:46 +0300

    fix before review

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -7,7 +7,7 @@ from api.views import PostViewSet, GroupViewSet, CommentViewSet
 router = DefaultRouter()
 router.register('api/v1/posts', PostViewSet)
 router.register('api/v1/groups', GroupViewSet)
-router.register('api/v1/posts/(?P<post_id>\d+)/comments',
+router.register(r'api/v1/posts/(?P<post_id>\d+)/comments',
                 CommentViewSet, basename='comments')
 
 urlpatterns = [
