ae458e90ca88ed397e4db35f836863da7a983bf3
coherentus <coherentus@gmail.com>
2021-07-28 01:05:07 +0300

    fix flake8

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -11,7 +11,7 @@ router_v1.register('groups', GroupViewSet, basename='groups')
 router_v1.register(r'posts/(?P<post_id>\d+)/comments',
                    CommentViewSet, basename='comments')
 
-urlpatterns = [    
+urlpatterns = [
     path('v1/api-token-auth/', views.obtain_auth_token),
-    path('v1/', include(router_v1.urls)),    
+    path('v1/', include(router_v1.urls)),
 ]
